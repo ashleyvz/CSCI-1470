3765cf242f851b479d8b6e54560a753b610414ec
Ashley Villela <avill0208@icloud.com>
2026-09-01 13:32:34 -0500

    fah

--- celsiustofahrenheit.cpp
+++ celsiustofahrenheit.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    double fahrenheit, celsius;
+    
+    cout << "Enter an integer value representing the temperature in Celsius: ";
+    cin >> celsius;
+    
+    fahrenheit = ((9. / 5.) * celsius) + 32;
+    
+    cout << fahrenheit;
+    
+    
+    
+    
+    
+    return 0;
+}
